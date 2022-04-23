dd6f6eacb4819ecaff7c587fd0b557bd91fed5ea
vivekgarrg <85150074+vivekgarrg@users.noreply.github.com>
2022-04-23 16:42:30 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -12,104 +12,112 @@ function App() {
 
 
   useEffect(()=>{
-    let res = JSON.parse(localStorage.getItem("tasks"))
-    let resDone = JSON.parse(localStorage.getItem("taskDone"))
+    let res = JSON.parse(localStorage.getItem("tasks"))  //Tasks-list
+    let resDone = JSON.parse(localStorage.getItem("taskDone")) //Task-Done-List 
     
-         if(res!=null){
-          let response = res.reverse();
-          setData(response.map((elem, idx)=>{
-                return(
-                    <div className='one-item' key={idx}>
-                      <input type='checkbox' onClick={(e) =>handleInput1(e,idx)} />
-                      <p>{elem}</p>
-                    </div>
-             
-                )
-              }));
-            }
-
-              
-
-          if(resDone!=null){
-           setTaskDone(resDone.map((ele, idx)=>{
-                  return(
-                    <div className='two-item' key={idx}>
-                      <input className='check' type="checkbox" checked={true} />
-                      <p  className='text'>{ele}</p>
-                    </div>
-                  )
-                    
-              }));
-      
-            }
-
-
+            if(res!=null){
+              let response = res.reverse();
+              setData(response.map((elem, idx)=>{
+                    return(
+                        <div className='one-item' key={idx}>
+                          <input type='checkbox' onClick={(e) =>handleInput1(e,idx)} />
+                          <p>{elem}</p>
+                        </div>
+                
+                    )
+                  }));
+                }else{
+                  setData('')
+                }
+          
+              if(resDone!=null){
+              setTaskDone(resDone.map((ele, idx)=>{
+                      return(
+                        <div className='two-item' key={idx}>
+                          <input className='check' type="checkbox" checked={true} />
+                          <p  className='text'>{ele}</p>
+                        </div>
+                      )
+                        
+                  }));
+          
+                }else{
+                  setTaskDone('')
+                }
           })
 
 
  const handleInput1 =(e, id)=>{
-  if(e.target.checked){
-        let response = JSON.parse(localStorage.getItem("tasks"))
-        const res = response.reverse()
-        let newArr = res.filter((elem,idx)=>idx!=id)
-        localStorage.setItem("tasks", JSON.stringify(newArr));
-        let result = localStorage.getItem("taskDone");
-            if(result == null){
-              let parr = []
-              parr.push(res[id])
-              localStorage.setItem("taskDone",JSON.stringify(parr));
-            }else{
-              let arr = [...JSON.parse(result), res[id]]
-              localStorage.setItem("taskDone",JSON.stringify(arr));
-            }
-      setTask('')
-                    }
-                    
-      e.target.checked = false
+      if(e.target.checked){
+          let response = JSON.parse(localStorage.getItem("tasks"))
+          const res = response.reverse()
+          let newArr = res.filter((elem,idx)=>idx!=id)
+          localStorage.setItem("tasks", JSON.stringify(newArr));
+          let result = localStorage.getItem("taskDone");
+                if(result == null){
+                  let parr = []
+                  parr.push(res[id])
+                  localStorage.setItem("taskDone",JSON.stringify(parr));
+                }else{
+                  let arr = [...JSON.parse(result), res[id]]
+                  localStorage.setItem("taskDone",JSON.stringify(arr));
+            
+        }
+      setTask('');
+   }
+    e.target.checked = false;
 }
   const handleInput=(e)=>{ 
         if(e.key==='Enter'){
           let result = localStorage.getItem("tasks");
-                if(result == null){
-                  let parr = []
-                  parr.push(task)
-                  localStorage.setItem("tasks",JSON.stringify(parr));
-                  let finalResult = JSON.parse(localStorage.getItem('tasks'));
-                  setTask('')
-                  }else{
-                    let arr = [...JSON.parse(result),task]
-                    localStorage.setItem("tasks",JSON.stringify(arr));
-                    let finalResult = JSON.parse(result);
-                    setTask('')
-                  }
-      }
+                    if(result == null){
+                      let parr = []
+                      parr.push(task)
+                      localStorage.setItem("tasks",JSON.stringify(parr));
+                      let finalResult = JSON.parse(localStorage.getItem('tasks'));
+                      setTask('')
+                      }else{
+                        let arr = [...JSON.parse(result),task]
+                        localStorage.setItem("tasks",JSON.stringify(arr));
+                        let finalResult = JSON.parse(result);
+                        setTask('')
+                      }
+          }
   }
+  const handleClear =()=>{
+        localStorage.removeItem('tasks')
+        localStorage.removeItem('taskDone')
+        setTask('')
+      }
+
 
   return (
   <div>
-    <div className="wrapper">
-      <div className="task-input">
-        <img src={icon} alt="icon"/>
-        <input type='text' value={task} onChange={(e)=>setTask(e.target.value)} onKeyDown={handleInput} placeholder="Add a new task.." />
-      </div>
-      <div className="task-heading">
-        <div className="pending">
-          <span >Tasks</span>
+        <div className='navbar'>
+          <h1>To-Do App</h1>
         </div>
-        <div className='btn'>
-        <button className="clear-btn">Clear All</button>
+        <div className="wrapper">
+              <div className="task-input">
+                <img src={icon} alt="icon"/>
+                <input type='text' value={task} onChange={(e)=>setTask(e.target.value)} onKeyDown={handleInput} placeholder="Add a new task.." />
+              </div>
+        <div className="task-heading">
+            <div className="pending">
+              <span >Tasks</span>
+            </div>
+            <div className='btn'>
+            <button onClick={handleClear} className="clear-btn">Clear All</button>
+            </div>
         </div>
-      </div>
-      <br/><hr/>
-       <div className="task-box">
-         {data==""&& taskDone==""?<p>You don't have any task here.</p>:<>{data}{taskDone}</>}
-       </div>
-      
 
+                       <br/><hr/>
+
+            <div className="task-box">
+              {data==""&& taskDone==""?<p>You don't have any task here.</p>:<>{data}{taskDone}</>}
+            </div>
+              <p style={{textAlign:"center"}}>@Made By Vivek ❤️</p>
+        </div>
     </div>
-
-
-      </div>
      
       
   );
